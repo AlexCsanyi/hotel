0ad8ff70bb35ae0b5e92c904071538bd5bf84f8a
AlexCsanyi <alex.csanyi@gmail.com>
2019-12-07 15:42:16 +0000

    services and title components added

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,16 +2,23 @@ import React from "react";
 import Hero from "../components/hero";
 import Banner from "../components/banner";
 import { Link } from "react-router-dom";
+import Services from "../components/services";
 
 const Home = () => {
   return (
-    <Hero hero="defaultHero">
-      <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at £139">
-        <Link to="/rooms" className="btn-primary">
-          Our Rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <React.Fragment>
+      <Hero hero="defaultHero">
+        <Banner
+          title="Luxurious Rooms"
+          subtitle="Deluxe Rooms starting at £139"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services></Services>
+    </React.Fragment>
   );
 };
 
--- src/pages/rooms.js
+++ src/pages/rooms.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Hero from "../components/hero";
+import Banner from "../components/banner";
+import { Link } from "react-router-dom";
 
 const Rooms = () => {
-  return <Hero hero="roomsHero"></Hero>;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="Our Rooms">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
