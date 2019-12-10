344f93bbb546c09fecff53418581fbe2c189d23b
AlexCsanyi <alex.csanyi@gmail.com>
2019-12-10 05:39:23 +0000

    finished roomList component, worked on handleChange function

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,5 +1,53 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "./title";
 
-export default function RoomFilter() {
-  return <div>Hello</div>;
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  let types = getUnique(rooms, "type");
+  types = ["all", ...types];
+  types = types.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="Search Rooms"></Title>
+      <form className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type"></label>
+          <select
+            onChange={handleChange}
+            className="form-control"
+            name="type"
+            id="type"
+            value={type}
+          >
+            {types}
+          </select>
+        </div>
+      </form>
+    </section>
+  );
 }
