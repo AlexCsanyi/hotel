3caaa38b5cd791805bc8283e768a18151314bc78
AlexCsanyi <alex.csanyi@gmail.com>
2019-12-16 06:51:09 +0000

    started woring on map component

--- src/components/map.js
+++ src/components/map.js
@@ -1,14 +1,69 @@
 import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import { GiFootprint } from "react-icons/gi";
 
 export default function Map() {
-  const [viewport] = useState({
-    latitude: 49.3988,
-    longitude: 8.6724,
-    zoom: 10,
+  const [viewport, setViewport] = useState({
+    latitude: 49.409,
+    longitude: 8.6733,
+    zoom: 14,
     width: "100%",
-    height: "900px"
+    height: "500px",
+    defaultMap: true
   });
 
-  return <ReactMapGl {...viewport}>Markers</ReactMapGl>;
+  const markerData = {
+    mark: [
+      {
+        id: "AA1",
+        name: "The Heidelberg House",
+        latitude: 49.409,
+        longitude: 8.6733,
+        description:
+          "We are located in a busy area known for hotels, restaurants & cafes, plus Altes Hallenbad & art at Sammlung Prinzhorn."
+      }
+    ]
+  };
+
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  return (
+    <React.Fragment>
+      <div className="map-nav"></div>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => setViewport(viewport)}
+      >
+        {markerData.mark.map(point => (
+          <Marker
+            key={point.id}
+            latitude={point.latitude}
+            longitude={point.longitude}
+          >
+            <GiFootprint
+              className="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedMarker(point);
+              }}
+            ></GiFootprint>
+          </Marker>
+        ))}
+
+        {selectedMarker ? (
+          <Popup
+            latitude={selectedMarker.latitude}
+            longitude={selectedMarker.longitude}
+            onClose={() => setSelectedMarker(null)}
+          >
+            <div className="marker-popup">
+              <h4>{selectedMarker.name}</h4>
+              <p>{selectedMarker.description}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGl>
+    </React.Fragment>
+  );
 }
